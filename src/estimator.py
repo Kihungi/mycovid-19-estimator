3977aed68ae499dd304ef3836fab7dbebec7863e
Kihungi <42550493+Kihungi@users.noreply.github.com>
2020-04-14 13:22:14 +0300

    Update estimator.py

--- src/estimator.py
+++ src/estimator.py
@@ -1,23 +1,22 @@
 import _init_
 
+  _init_.estimator(impact):
+    impact.currentlyInfected = reportedCases * 10
+    factor = int(totalDays/3)
+    impact.infectionsByRequestedTime = currentlyInfected * 2^factor
+    impact.severeCasesByRequestedTime = 0.15 * infectionsByRequestedTime
+    impact.hospitalBedsByRequestedTime= totalHospitalBeds - severeCasesByRequestedTime
+    impact.casesForICUByRequestedTime = 0.05 * infectionsByRequestedTime
+    impact.casesForVentilatorsByRequestedTime = 0.02 * infectionsByRequestedTime
+    impact.dollarsInFlight = infectionsByRequestedTime * avgDailyIncomeInUSD * totalDays
+  return impact
 
-  monthWeeks = month * 4
-  weekDays = week * 7
-  totalDays = monthWeeks + weekDays + days
-  impact.currentlyInfected = reportedCases * 10
-  factor = int(totalDays/3)
-  impact.infectionsByRequestedTime = currentlyInfected * 2^factor
-  impact.severeCasesByRequestedTime = 0.15 * infectionsByRequestedTime
-  impact.hospitalBedsByRequestedTime= totalHospitalBeds - severeCasesByRequestedTime
-  impact.casesForICUByRequestedTime = 0.05 * infectionsByRequestedTime
-  impact.casesForVentilatorsByRequestedTime = 0.02 * infectionsByRequestedTime
-  impact.dollarsInFlight = infectionsByRequestedTime * avgDailyIncomeInUSD * totalDays
-
-  severeImpact.currentlyInfected = reportedCases * 50
-  severeImpact.infectionsByRequestedTime = currentlyInfected * 2^factor
-  severeImpact.severeCasesByRequestedTime = 0.15 * infectionsByRequestedTime
-  severeImpact.hospitalBedsByRequestedTime= totalHospitalBeds - severeCasesByRequestedTime
-  severeImpact.casesForICUByRequestedTime = 0.05 * infectionsByRequestedTime
-  severeImpact.casesForVentilatorsByRequestedTime = 0.02 * infectionsByRequestedTime
-  severeImpact.dollarsInFlight = infectionsByRequestedTime * avgDailyIncomePopulation * avgDailyIncomeInUSD * totalDays
-
+ _init_.estimator(severeImpact):
+   severeImpact.currentlyInfected = reportedCases * 50
+   severeImpact.infectionsByRequestedTime = currentlyInfected * 2^factor
+   severeImpact.severeCasesByRequestedTime = 0.15 * infectionsByRequestedTime
+   severeImpact.hospitalBedsByRequestedTime= totalHospitalBeds - severeCasesByRequestedTime
+   severeImpact.casesForICUByRequestedTime = 0.05 * infectionsByRequestedTime
+   severeImpact.casesForVentilatorsByRequestedTime = 0.02 * infectionsByRequestedTime
+   severeImpact.dollarsInFlight = infectionsByRequestedTime * avgDailyIncomePopulation * avgDailyIncomeInUSD * totalDays
+ return severeImpact
