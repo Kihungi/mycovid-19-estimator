ae072375733cc9b4c686c4c4edc35cc1dd3f363f
Kihungi <42550493+Kihungi@users.noreply.github.com>
2020-04-14 13:46:24 +0300

    Update estimator.py

--- src/estimator.py
+++ src/estimator.py
@@ -1,10 +1,10 @@
 
 import _init_
 
- _init_.estimator(data):
- return data
+_init_.estimator(data):
+return data
 
- _init_.estimator(impact):
+_init_.estimator(impact):
     impact.currentlyInfected = reportedCases * 10
     factor = int(totalDays/3)
     impact.infectionsByRequestedTime = currentlyInfected * 2^factor
@@ -13,9 +13,9 @@ import _init_
     impact.casesForICUByRequestedTime = 0.05 * infectionsByRequestedTime
     impact.casesForVentilatorsByRequestedTime = 0.02 * infectionsByRequestedTime
     impact.dollarsInFlight = infectionsByRequestedTime * avgDailyIncomeInUSD * totalDays
- return impact
+return impact
 
- _init_.estimator(severeImpact):
+_init_.estimator(severeImpact):
    severeImpact.currentlyInfected = reportedCases * 50
    severeImpact.infectionsByRequestedTime = currentlyInfected * 2^factor
    severeImpact.severeCasesByRequestedTime = 0.15 * infectionsByRequestedTime
@@ -23,5 +23,5 @@ import _init_
    severeImpact.casesForICUByRequestedTime = 0.05 * infectionsByRequestedTime
    severeImpact.casesForVentilatorsByRequestedTime = 0.02 * infectionsByRequestedTime
    severeImpact.dollarsInFlight = infectionsByRequestedTime * avgDailyIncomePopulation * avgDailyIncomeInUSD * totalDays
- return severeImpact
+return severeImpact
 
