1b799020d1b4fb230db2528215cb217189e7be64
Kihungi <42550493+Kihungi@users.noreply.github.com>
2020-04-15 11:22:39 +0300

    Update estimator.py

--- src/estimator.py
+++ src/estimator.py
@@ -4,24 +4,25 @@ import _init_
 def estimator(data):
  return data
 
-def estimator(impact):
-    impact.currentlyInfected = reportedCases * 10
-    factor = int(totalDays/3)
-    impact.infectionsByRequestedTime = currentlyInfected * 2^factor
-    impact.severeCasesByRequestedTime = 0.15 * infectionsByRequestedTime
-    impact.hospitalBedsByRequestedTime= totalHospitalBeds - severeCasesByRequestedTime
-    impact.casesForICUByRequestedTime = 0.05 * infectionsByRequestedTime
-    impact.casesForVentilatorsByRequestedTime = 0.02 * infectionsByRequestedTime
-    impact.dollarsInFlight = infectionsByRequestedTime * avgDailyIncomeInUSD * totalDays
-    return impact
+factor = int(days/3)
 
-def estimator(severeImpact):
-   severeImpact.currentlyInfected = reportedCases * 50
-   severeImpact.infectionsByRequestedTime = currentlyInfected * 2^factor
-   severeImpact.severeCasesByRequestedTime = 0.15 * infectionsByRequestedTime
-   severeImpact.hospitalBedsByRequestedTime= totalHospitalBeds - severeCasesByRequestedTime
-   severeImpact.casesForICUByRequestedTime = 0.05 * infectionsByRequestedTime
-   severeImpact.casesForVentilatorsByRequestedTime = 0.02 * infectionsByRequestedTime
-   severeImpact.dollarsInFlight = infectionsByRequestedTime * avgDailyIncomePopulation * avgDailyIncomeInUSD * totalDays
-   return severeImpact
 
+impact.update({
+    "currentlyInfected" : reportedCases * 10,
+    "infectionsByRequestedTime" : currentlyInfected * 2^factor,
+    "severeCasesByRequestedTime" : 0.15 * infectionsByRequestedTime
+    "hospitalBedsByRequestedTime" : totalHospitalBeds - severeCasesByRequestedTime
+    "casesForICUByRequestedTime" : 0.05 * infectionsByRequestedTime
+    "casesForVentilatorsByRequestedTime" : 0.02 * infectionsByRequestedTime
+    "dollarsInFlight" : infectionsByRequestedTime * avgDailyIncomeInUSD * days
+})
+
+severeImpact.update({
+    "currentlyInfected" : reportedCases * 50,
+    "infectionsByRequestedTime" : currentlyInfected * 2^factor,
+    "severeCasesByRequestedTime" : 0.15 * infectionsByRequestedTime
+    "hospitalBedsByRequestedTime" : totalHospitalBeds - severeCasesByRequestedTime
+    "casesForICUByRequestedTime" : 0.05 * infectionsByRequestedTime
+    "casesForVentilatorsByRequestedTime" : 0.02 * infectionsByRequestedTime
+    "dollarsInFlight" : infectionsByRequestedTime * avgDailyIncomeInUSD * days
+})
