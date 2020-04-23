3d2437e4908b25278102e7e29260d29fb5113aba
Kihungi <42550493+Kihungi@users.noreply.github.com>
2020-04-23 11:29:54 +0300

    Update estimator.py

--- src/estimator.py
+++ src/estimator.py
@@ -6,11 +6,11 @@ data = {
   "avgIncomeInUSD": 4,
   "avgDailyIncomePopulationInUSD": 0.71
  }
- periodType = "days"
- timeToElapse = 58
- reportedCases = 674
- population = 66622705  
- totalHospitalBeds = 1380614
+ "periodType": "days",
+ "timeToElapse": 58,
+ "reportedCases": 674,
+ "population": 66622705,  
+ "totalHospitalBeds": 1380614
 }
 
 for x in data:
@@ -18,25 +18,27 @@ for x in data:
  
 
 def estimator(data):
+ days = 365
+ factor = int(days/3)
  estimate = {
-  factor = int(days/3)
+  
   impact: {
-     "currentlyInfected" : reportedCases * 10,
-     "infectionsByRequestedTime" : currentlyInfected * 2^factor,
-     "severeCasesByRequestedTime" : 0.15 * infectionsByRequestedTime
-     "hospitalBedsByRequestedTime" : totalHospitalBeds - severeCasesByRequestedTime
-     "casesForICUByRequestedTime" : 0.05 * infectionsByRequestedTime
-     "casesForVentilatorsByRequestedTime" : 0.02 * infectionsByRequestedTime
-     "dollarsInFlight" : infectionsByRequestedTime * avgDailyIncomeInUSD * days
+     "currentlyInfected" : "reportedCases" * 10,
+     "infectionsByRequestedTime" : "currentlyInfected" * 2^factor,
+     "severeCasesByRequestedTime" : 0.15 * "infectionsByRequestedTime",
+     "hospitalBedsByRequestedTime" : "totalHospitalBeds" - "severeCasesByRequestedTime",
+     "casesForICUByRequestedTime" : 0.05 * "infectionsByRequestedTime",
+     "casesForVentilatorsByRequestedTime" : 0.02 * "infectionsByRequestedTime",
+     "dollarsInFlight" : "infectionsByRequestedTime" * "avgDailyIncomeInUSD" * days
   }
   severeImpact: {
-    "currentlyInfected" : reportedCases * 50,
-    "infectionsByRequestedTime" : currentlyInfected * 2^factor,
-    "severeCasesByRequestedTime" : 0.15 * infectionsByRequestedTime
-    "hospitalBedsByRequestedTime" : totalHospitalBeds - severeCasesByRequestedTime
-    "casesForICUByRequestedTime" : 0.05 * infectionsByRequestedTime
-    "casesForVentilatorsByRequestedTime" : 0.02 * infectionsByRequestedTime
-    "dollarsInFlight" : infectionsByRequestedTime * avgDailyIncomeInUSD * days
+    "currentlyInfected" : "reportedCases" * 50,
+    "infectionsByRequestedTime" : "currentlyInfected" * 2^factor,
+    "severeCasesByRequestedTime" : 0.15 * "infectionsByRequestedTime",
+    "hospitalBedsByRequestedTime" : "totalHospitalBeds" - "severeCasesByRequestedTime",
+    "casesForICUByRequestedTime" : 0.05 * "infectionsByRequestedTime",
+    "casesForVentilatorsByRequestedTime" : 0.02 * "infectionsByRequestedTime",
+    "dollarsInFlight" : "infectionsByRequestedTime" * "avgDailyIncomeInUSD" * days
   }
  }
  return data
